79bca3e6dc0b6ee72f1fbff40ef646a8ee3663be
Cam <126632693+dreddisdead@users.noreply.github.com>
2023-10-03 11:33:53 -0400

    file with custom theme added

---
+++ user.py
@@ -0,0 +1,23 @@
+USER_THEMES = {
+    "deardiary": {
+        "type": "light",
+        "colors": {
+            "primary": "#c7a17d",
+            "secondary": "#7f5539",
+            "success": "#ddb892",
+            "info": "#b08968",
+            "warning": "#e2c2a2",
+            "danger": "#9c6644",
+            "light": "#ead6c3",
+            "dark": "#7f5539",
+            "bg": "#986f51",
+            "fg": "#ead6c3",
+            "selectbg": "#ddb892",
+            "selectfg": "#ede0d4",
+            "border": "#ddb892",
+            "inputfg": "#ede0d4",
+            "inputbg": "#7f5539",
+            "active": "#e5e5e5"
+        }
+    }
+}
